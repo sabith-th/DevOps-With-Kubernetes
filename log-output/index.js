1da9645a136c6352a366cb9ab8f1685e436b6c3a
Sabith T H <sabith.th.07@gmail.com>
2022-03-08 19:05:07 +0100

    Completed Exercise 1.07

--- log-output/index.js
+++ log-output/index.js
@@ -1,8 +1,21 @@
+const Koa = require("koa");
+const app = new Koa();
+const PORT = process.env.PORT || 3000;
+
 const randomString = Math.random().toString(36).substring(2);
+let current;
 
 printLog = () => {
-  console.log(`${new Date()}: ${randomString}`);
+  current = `${new Date()}: ${randomString}`;
+  console.log(current);
   setTimeout(printLog, 5000);
 };
 
 printLog();
+
+app.use(async (ctx) => {
+  ctx.response.body = current;
+});
+
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
