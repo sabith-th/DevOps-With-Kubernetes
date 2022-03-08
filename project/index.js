0ced3efab30413dee2ed0bc81697e9dc450d3276
Sabith T H <sabith.th.07@gmail.com>
2022-03-08 16:28:09 +0100

    Completed Exercise 1.05 and 1.06

--- project/index.js
+++ project/index.js
@@ -2,5 +2,9 @@ const Koa = require("koa");
 const app = new Koa();
 const PORT = process.env.PORT || 3000;
 
+app.use(async (ctx) => {
+  ctx.response.body = "<h1>Hello World</h1>";
+});
+
 app.listen(PORT);
 console.log(`Listening on port ${PORT}`);
